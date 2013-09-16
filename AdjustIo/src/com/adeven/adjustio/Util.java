cfd4639233c87836b35b267f82f6bd8c3631312e
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2013-09-16 17:10:45 +0200

    Update version to 2.1.0

--- AdjustIo/src/com/adeven/adjustio/Util.java
+++ AdjustIo/src/com/adeven/adjustio/Util.java
@@ -37,7 +37,7 @@ import android.util.DisplayMetrics;
  */
 public class Util {
     protected static final String BASE_URL   = "https://app.adjust.io";
-    protected static final String CLIENT_SDK = "android2.0.1";
+    protected static final String CLIENT_SDK = "android2.1.0";
 
     private static final String UNKNOWN = "unknown";
 
