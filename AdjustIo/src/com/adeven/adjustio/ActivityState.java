c531a2828c92dc30ccaf0d2372f18ef1dcb88bc2
Benjamin Weiss <benjamin.weiss@immobilienscout24.de>
2013-11-08 13:25:29 +0100

    formatting

--- AdjustIo/src/com/adeven/adjustio/ActivityState.java
+++ AdjustIo/src/com/adeven/adjustio/ActivityState.java
@@ -21,7 +21,7 @@ public class ActivityState implements Serializable {
     protected int sessionCount;
 
     // session attributes
-    protected int subsessionCount;
+    protected int  subsessionCount;
     protected long sessionLength;   // all durations in milliseconds
     protected long timeSpent;
     protected long lastActivity;    // all times in milliseconds since 1970
@@ -30,23 +30,23 @@ public class ActivityState implements Serializable {
     protected long lastInterval;
 
     protected ActivityState() {
-        eventCount      =  0; // no events yet
-        sessionCount    =  0; // the first session just started
-        subsessionCount = -1; // we don't know how many subssessions this first  session will have
-        sessionLength   = -1; // same for session length and time spent
-        timeSpent       = -1; // this information will be collected and attached to the next session
-        lastActivity    = -1;
-        createdAt       = -1;
-        lastInterval    = -1;
+        eventCount = 0; // no events yet
+        sessionCount = 0; // the first session just started
+        subsessionCount = -1; // we don't know how many subsessions this first  session will have
+        sessionLength = -1; // same for session length and time spent
+        timeSpent = -1; // this information will be collected and attached to the next session
+        lastActivity = -1;
+        createdAt = -1;
+        lastInterval = -1;
     }
 
     protected void resetSessionAttributes(long now) {
         subsessionCount = 1; // first subsession
-        sessionLength   = 0; // no session length yet
-        timeSpent       = 0; // no time spent yet
-        lastActivity    = now;
-        createdAt       = -1;
-        lastInterval    = -1;
+        sessionLength = 0; // no session length yet
+        timeSpent = 0; // no time spent yet
+        lastActivity = now;
+        createdAt = -1;
+        lastInterval = -1;
     }
 
     protected void injectSessionAttributes(PackageBuilder builder) {
@@ -61,26 +61,26 @@ public class ActivityState implements Serializable {
 
     public String toString() {
         return String.format(Locale.US,
-                "ec:%d sc:%d ssc:%d sl:%.1f ts:%.1f la:%s",
-                eventCount, sessionCount, subsessionCount,
-                sessionLength / 1000.0, timeSpent / 1000.0,
-                stamp(lastActivity));
+                             "ec:%d sc:%d ssc:%d sl:%.1f ts:%.1f la:%s",
+                             eventCount, sessionCount, subsessionCount,
+                             sessionLength / 1000.0, timeSpent / 1000.0,
+                             stamp(lastActivity));
     }
 
     private static String stamp(long dateMillis) {
         Date date = new Date(dateMillis);
         return String.format(Locale.US,
-                "%02d:%02d:%02d",
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds());
+                             "%02d:%02d:%02d",
+                             date.getHours(),
+                             date.getMinutes(),
+                             date.getSeconds());
     }
 
     private void injectGeneralAttributes(PackageBuilder builder) {
-        builder.sessionCount    = sessionCount;
+        builder.sessionCount = sessionCount;
         builder.subsessionCount = subsessionCount;
-        builder.sessionLength   = sessionLength;
-        builder.timeSpent       = timeSpent;
-        builder.createdAt       = createdAt;
+        builder.sessionLength = sessionLength;
+        builder.timeSpent = timeSpent;
+        builder.createdAt = createdAt;
     }
 }
