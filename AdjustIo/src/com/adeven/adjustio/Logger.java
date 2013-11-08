a33dcb46b13d6af2a890e9fd5ecc0c0e02a92ada
Benjamin Weiss <benjamin.weiss@immobilienscout24.de>
2013-11-08 14:47:33 +0100

    introduces LogLevel wrapper

--- AdjustIo/src/com/adeven/adjustio/Logger.java
+++ AdjustIo/src/com/adeven/adjustio/Logger.java
@@ -21,50 +21,73 @@ public class Logger {
         LogLevel(final int androidLogLevel) {
             this.androidLogLevel = androidLogLevel;
         }
-        
+
         public int getAndroidLogLevel() {
             return androidLogLevel;
         }
-
+        
+        public boolean isEnabled() {
+             return androidLogLevel <= Logger.androidLogLevel;
+        }
+        
     }
 
-    protected static final String LOGTAG   = "AdjustIo";
-    private static         int    logLevel = Log.INFO;
+    protected static final String   LOGTAG          = "AdjustIo";
+    private static         int      androidLogLevel = Log.INFO;
+    private static         LogLevel logLevel        = LogLevel.INFO;
 
-    protected static void setLogLevel(int logLevel) {
+    protected static void setLogLevel(LogLevel logLevel) {
         Logger.logLevel = logLevel;
+        Logger.androidLogLevel = logLevel.getAndroidLogLevel();
+    }
+
+    /**
+     * Use {@link Logger#setLogLevel(com.adeven.adjustio.Logger.LogLevel)} instead
+     *
+     * @param androidLogLevel
+     */
+    @Deprecated
+    protected static void setLogLevel(int androidLogLevel) {
+        Logger.androidLogLevel = androidLogLevel;
+        //For the sake of compatibility. Only required until setLogLevel is being removed.
+        for (LogLevel logLevel : LogLevel.values()) {
+            if (logLevel.androidLogLevel == androidLogLevel) {
+                Logger.logLevel = logLevel;
+                return;
+            }
+        }
     }
 
     protected static int getLogLevel() {
-        return logLevel;
+        return androidLogLevel;
     }
 
     protected static void verbose(String message) {
-        if (logLevel <= Log.VERBOSE) {
+        if (logLevel.isEnabled()) {
             Log.v(LOGTAG, message);
         }
     }
 
     protected static void debug(String message) {
-        if (logLevel <= Log.DEBUG) {
+        if (logLevel.isEnabled()) {
             Log.d(LOGTAG, message);
         }
     }
 
     protected static void info(String message) {
-        if (logLevel <= Log.INFO) {
+        if (logLevel.isEnabled()) {
             Log.i(LOGTAG, message);
         }
     }
 
     protected static void warn(String message) {
-        if (logLevel <= Log.WARN) {
+        if (logLevel.isEnabled()) {
             Log.w(LOGTAG, message);
         }
     }
 
     protected static void error(String message) {
-        if (logLevel <= Log.ERROR) {
+        if (logLevel.isEnabled()) {
             Log.e(LOGTAG, message);
         }
     }
