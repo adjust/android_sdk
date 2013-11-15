ea4fa2042f7349aa8bf633ec1ed41279221776aa
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2013-11-15 18:07:49 +0100

    Make Logger methods public

--- AdjustIo/src/com/adeven/adjustio/Logger.java
+++ AdjustIo/src/com/adeven/adjustio/Logger.java
@@ -30,41 +30,41 @@ public class Logger {
 
     private static LogLevel logLevel = LogLevel.INFO;
 
-    protected static void setLogLevel(LogLevel logLevel) {
+    public static void setLogLevel(LogLevel logLevel) {
         Logger.logLevel = logLevel;
     }
 
-    protected static void verbose(String message) {
+    public static void verbose(String message) {
         if (logLevel.androidLogLevel <= Log.VERBOSE) {
             Log.v(LOGTAG, message);
         }
     }
 
-    protected static void debug(String message) {
+    public static void debug(String message) {
         if (logLevel.androidLogLevel <= Log.DEBUG) {
             Log.d(LOGTAG, message);
         }
     }
 
-    protected static void info(String message) {
+    public static void info(String message) {
         if (logLevel.androidLogLevel <= Log.INFO) {
             Log.i(LOGTAG, message);
         }
     }
 
-    protected static void warn(String message) {
+    public static void warn(String message) {
         if (logLevel.androidLogLevel <= Log.WARN) {
             Log.w(LOGTAG, message);
         }
     }
 
-    protected static void error(String message) {
+    public static void error(String message) {
         if (logLevel.androidLogLevel <= Log.ERROR) {
             Log.e(LOGTAG, message);
         }
     }
 
-    protected static void Assert(String message) {
+    public static void Assert(String message) {
         Log.println(Log.ASSERT, LOGTAG, message);
     }
 }
