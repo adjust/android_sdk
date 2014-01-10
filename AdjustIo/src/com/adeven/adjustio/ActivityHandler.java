1c0ff0d06729e734da06cd609fd333cad2f1ab73
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-01-10 16:30:48 +0100

    Add parameter AdjustIoDropOfflineActivities to set ActivityHandler.dropOfflineActivities

--- AdjustIo/src/com/adeven/adjustio/ActivityHandler.java
+++ AdjustIo/src/com/adeven/adjustio/ActivityHandler.java
@@ -58,6 +58,7 @@ public class ActivityHandler extends HandlerThread {
     private        String                   environment;
     private        String                   defaultTracker;
     private        boolean                  eventBuffering;
+    private        boolean                  dropOfflineActivities;
 
     private String appToken;
     private String macSha1;
@@ -507,6 +508,7 @@ public class ActivityHandler extends HandlerThread {
         setDefaultTracker(bundle.getString("AdjustIoDefaultTracker"));
         setEventBuffering(bundle.getBoolean("AdjustIoEventBuffering"));
         Logger.setLogLevelString(bundle.getString("AdjustIoLogLevel"));
+        setDropOfflineActivities(bundle.getBoolean("AdjustIoDropOfflineActivities"));
     }
 
     private void setEnvironment(String env) {
@@ -540,6 +542,13 @@ public class ActivityHandler extends HandlerThread {
         defaultTracker = tracker;
         if (defaultTracker != null) {
             Logger.info(String.format("Default tracker: '%s'", defaultTracker));
+        }
+    }
+
+    private void setDropOfflineActivities(boolean drop) {
+        dropOfflineActivities = drop;
+        if (dropOfflineActivities) {
+            Logger.info("Offline activities will get dropped");
         }
     }
 
