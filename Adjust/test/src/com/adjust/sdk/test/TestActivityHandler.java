8d98ac0f55ca77a4586c180070cabc10a73c20dd
Pedro <pedro@adeven.com>
2014-06-30 17:36:51 +0200

    New version

--- Adjust/test/src/com/adjust/sdk/test/TestActivityHandler.java
+++ Adjust/test/src/com/adjust/sdk/test/TestActivityHandler.java
@@ -93,7 +93,7 @@ public class TestActivityHandler extends ActivityInstrumentationTestCase2<UnitTe
 
         // check the Sdk version is being tested
         assertEquals(activityPackage.getExtendedString(),
-            "android3.3.4", activityPackage.getClientSdk());
+            "android3.3.5", activityPackage.getClientSdk());
 
         // check the server url
         assertEquals(Constants.BASE_URL, "https://app.adjust.io");
