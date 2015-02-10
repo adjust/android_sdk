2b574f0d7edc2c2def1af36cfe311a6395b3d1e0
Pedro <pedro@adeven.com>
2015-02-10 17:32:21 +0100

    New version 4.0.0

--- Adjust/src/com/adjust/sdk/Constants.java
+++ Adjust/src/com/adjust/sdk/Constants.java
@@ -24,7 +24,7 @@ public interface Constants {
     String BASE_URL = "https://app.adjust.com";
     String SCHEME = "https";
     String AUTHORITY = "app.adjust.com";
-    String CLIENT_SDK = "android3.6.1";
+    String CLIENT_SDK = "android4.0.0";
     String LOGTAG = "Adjust";
 
     String SESSION_STATE_FILENAME = "AdjustIoActivityState";
