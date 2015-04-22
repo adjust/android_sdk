947fee45c4e4d7c4bbfd97161da8d76240a6f1bf
Pedro <pedro@adeven.com>
2015-04-22 18:20:15 +0200

    New version v4.0.4

--- Adjust/test/src/androidTest/java/com/adjust/sdk/test/TestActivityPackage.java
+++ Adjust/test/src/androidTest/java/com/adjust/sdk/test/TestActivityPackage.java
@@ -45,7 +45,7 @@ public class TestActivityPackage {
         // default values
         appToken = "123456789012";
         environment = "sandbox";
-        clientSdk = "android4.0.3";
+        clientSdk = "android4.0.4";
         suffix = "";
         attribution = new AdjustAttribution();
     }
