67b896e3ec54d8d05c47f80cf384f808c310f6b8
Pedro <pedro@adeven.com>
2015-06-05 15:17:52 +0200

    Include optional criteo partner id

--- Adjust/plugin/AdjustCriteo.java
+++ Adjust/plugin/AdjustCriteo.java
@@ -19,6 +19,7 @@ public class AdjustCriteo {
     private static String hashEmailInternal;
     private static String checkInDateInternal;
     private static String checkOutDateInternal;
+    private static String partnerIdInternal;
 
     public static void injectViewListingIntoEvent(AdjustEvent event, List<String> productIds, String customerId) {
         String jsonProducts = createCriteoVLFromProducts(productIds);
@@ -99,9 +100,14 @@ public class AdjustCriteo {
         checkOutDateInternal = checkOutDate;
     }
 
+    public static void injectPartnerIdIntoCriteoEvents(String partnerId) {
+        partnerIdInternal = partnerId;
+    }
+
     private static void injectOptionalParams(AdjustEvent event) {
         injectHashEmail(event);
         injectSearchDates(event);
+        injectPartnerId(event);
     }
     private static void injectHashEmail(AdjustEvent event) {
         if (hashEmailInternal == null || hashEmailInternal.isEmpty()) {
@@ -120,6 +126,15 @@ public class AdjustCriteo {
         event.addPartnerParameter("din", checkInDateInternal);
         event.addPartnerParameter("dout", checkOutDateInternal);
     }
+
+    private static void injectPartnerId(AdjustEvent event) {
+        if (partnerIdInternal == null || partnerIdInternal.isEmpty()) {
+            return;
+        }
+
+        event.addPartnerParameter("criteo_partner_id", partnerIdInternal);
+    }
+
 
     private static String createCriteoVLFromProducts(List<String> productIds) {
         if (productIds == null) {
