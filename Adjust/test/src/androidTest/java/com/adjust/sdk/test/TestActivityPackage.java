e8217df37bee0c33d1c8c14259b5ee799a551171
uerceg <ugljesa@adjust.com>
2015-12-21 17:24:26 +0100

    New version 4.1.6

--- Adjust/test/src/androidTest/java/com/adjust/sdk/test/TestActivityPackage.java
+++ Adjust/test/src/androidTest/java/com/adjust/sdk/test/TestActivityPackage.java
@@ -49,7 +49,7 @@ public class TestActivityPackage {
         // default values
         appToken = "123456789012";
         environment = "sandbox";
-        clientSdk = "android4.1.5";
+        clientSdk = "android4.1.6";
         suffix = "";
         attribution = new AdjustAttribution();
     }
