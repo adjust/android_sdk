c560ebd159051bc5427a814ba7fe3270e995d9db
Obaied <abdullah.obaied@gmail.com>
2017-02-09 14:07:12 +0100

    Remove redundant RunnableWrapper instantiations and put back log messages

--- Adjust/adjust/src/main/java/com/adjust/sdk/CustomScheduledExecutor.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/CustomScheduledExecutor.java
@@ -8,19 +8,26 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.*;
 
 /**
  * Created by pfms on 05/08/2016.
  */
 public final class CustomScheduledExecutor {
+    private String source;
     private ScheduledThreadPoolExecutor executor;
+    private final AtomicInteger threadCounter = new AtomicInteger(1);
+
 
     public CustomScheduledExecutor(final String source, boolean doKeepAlive) {
+        this.source = source;
+
         executor = new ScheduledThreadPoolExecutor(1,   // Single thread
                 new ThreadFactory() {                   // Creator of daemon threads
                     @Override
                     public Thread newThread(Runnable runnable) {
                         Thread thread = Executors.defaultThreadFactory().newThread(new RunnableWrapper(runnable));
+
                         thread.setPriority(Thread.MIN_PRIORITY);
                         thread.setName(Constants.THREAD_PREFIX + thread.getName() + "-" + source);
                         thread.setDaemon(true);
@@ -31,6 +38,8 @@ public final class CustomScheduledExecutor {
                                 AdjustFactory.getLogger().error("Thread %s with error %s", th.getName(), tr.getMessage());
                             }
                         });
+
+                        AdjustFactory.getLogger().verbose("Thread %s created", thread.getName());
                         return thread;
                     }
                 }, new RejectedExecutionHandler() {     // Logs rejected runnables rejected from the entering the pool
@@ -56,24 +65,37 @@ public final class CustomScheduledExecutor {
     }
 
     public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-        return executor.scheduleWithFixedDelay(new RunnableWrapper(command), initialDelay, delay, unit);
+        AdjustFactory.getLogger().verbose("CustomScheduledExecutor scheduleWithFixedDelay from %s source, with %d delay and %d initial delay",
+                source, delay, initialDelay);
+        return executor.scheduleWithFixedDelay(command, initialDelay, delay, unit);
     }
 
     public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-        return executor.schedule(new RunnableWrapper(command), delay, unit);
+        AdjustFactory.getLogger().verbose("CustomScheduledExecutor schedule from %s source, with %d delay", source, delay);
+        return executor.schedule(command, delay, unit);
     }
 
     private class RunnableWrapper implements Runnable {
         private Runnable runnable;
+//        private long created;
+//        private int threadNumber;
 
         public RunnableWrapper(Runnable runnable) {
             this.runnable = runnable;
+//            created = System.currentTimeMillis();
+//            threadNumber = threadCounter.getAndIncrement();
+//            AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s created at %d", threadNumber, source, created);
         }
 
         @Override
         public void run() {
             try {
+//                long before = System.currentTimeMillis();
+//                AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s source, before running at %d", threadNumber, source, before);
                 runnable.run();
+//                long after = System.currentTimeMillis();
+//                AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s source, after running at %d", threadNumber, source, after);
+
             } catch (Throwable t) {
                 AdjustFactory.getLogger().error("Runnable error %s", t.getMessage());
             }
