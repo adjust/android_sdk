e4b605570a510acdfceb974e0bbd7a4a45a3770f
nonelse <pedro@adjust.com>
2017-06-28 15:36:53 +0200

    Send first session package even with buffering

--- Adjust/adjust/src/main/java/com/adjust/sdk/ActivityHandler.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/ActivityHandler.java
@@ -1266,6 +1266,12 @@ public class ActivityHandler implements IActivityHandler {
 
         resumeSendingI();
 
+        // try to send if it's the first launch and it hasn't received the session response
+        //  even if event buffering is enabled
+        if (internalState.isFirstLaunch() && internalState.hasSessionResponseNotBeenProcessed()) {
+            packageHandler.sendFirstPackage();
+        }
+
         // try to send
         if (!adjustConfig.eventBufferingEnabled) {
             packageHandler.sendFirstPackage();
