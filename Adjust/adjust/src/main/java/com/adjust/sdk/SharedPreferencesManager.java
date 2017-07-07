e976cfc0b05d7e4314abb87bbf1bc279fe0bd40e
uerceg <ugi@adjust.com>
2017-07-07 21:14:53 +0100

    Checkstyle cleanup for SharedPreferencesManager

--- Adjust/adjust/src/main/java/com/adjust/sdk/SharedPreferencesManager.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/SharedPreferencesManager.java
@@ -7,50 +7,58 @@ import android.content.SharedPreferences;
  * Class used for shared preferences manipulation.
  */
 public class SharedPreferencesManager {
+    /**
+     * Shared preferences of the app.
+     */
     private SharedPreferences sharedPreferences;
 
     /**
-     * Default constructor
+     * Default constructor.
+     *
      * @param context Application context
-     * @param name Shared preferences name
+     * @param name    Shared preferences name
      */
-    public SharedPreferencesManager(Context context, String name) {
+    public SharedPreferencesManager(final Context context, final String name) {
         this.sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
     }
 
     /**
-     * Write a string value to shared preferences
-     * @param key Key to be written to shared preferences
+     * Write a string value to shared preferences.
+     *
+     * @param key   Key to be written to shared preferences
      * @param value Value to be written to shared preferences
      */
-    public synchronized void saveStringToSharedPreferences(String key, String value) {
+    public synchronized void saveStringToSharedPreferences(final String key, final String value) {
         this.sharedPreferences.edit().putString(key, value).apply();
     }
 
     /**
-     * Write a long value to shared preferences
-     * @param key Key to be written to shared preferences
+     * Write a long value to shared preferences.
+     *
+     * @param key   Key to be written to shared preferences
      * @param value Value to be written to shared preferences
      */
-    public synchronized void saveLongToSharedPreferences(String key, long value) {
+    public synchronized void saveLongToSharedPreferences(final String key, final long value) {
         this.sharedPreferences.edit().putLong(key, value).apply();
     }
 
     /**
-     * Write a boolean value to shared preferences
-     * @param key Key to be written to shared preferences
+     * Write a boolean value to shared preferences.
+     *
+     * @param key   Key to be written to shared preferences
      * @param value Value to be written to shared preferences
      */
-    public synchronized void saveBooleanToSharedPreferences(String key, boolean value) {
+    public synchronized void saveBooleanToSharedPreferences(final String key, final boolean value) {
         this.sharedPreferences.edit().putBoolean(key, value).apply();
     }
 
     /**
-     * Get a string value from shared preferences
+     * Get a string value from shared preferences.
+     *
      * @param key Key for which string value should be retrieved
      * @return String value for given key saved in shared preferences (null if not found)
      */
-    public synchronized String getStringFromSharedPreferences(String key) {
+    public synchronized String getStringFromSharedPreferences(final String key) {
         String defaultValue = null;
 
         try {
@@ -61,11 +69,12 @@ public class SharedPreferencesManager {
     }
 
     /**
-     * Get a long value from shared preferences
+     * Get a long value from shared preferences.
+     *
      * @param key Key for which long value should be retrieved
      * @return Long value for given key saved in shared preferences (-1 if not found)
      */
-    public synchronized long getLongFromSharedPreferences(String key) {
+    public synchronized long getLongFromSharedPreferences(final String key) {
         long defaultValue = -1;
 
         try {
@@ -76,11 +85,12 @@ public class SharedPreferencesManager {
     }
 
     /**
-     * Get a long value from shared preferences
+     * Get a long value from shared preferences.
+     *
      * @param key Key for which boolean value should be retrieved
      * @return Boolean value for given key saved in shared preferences (false if not found)
      */
-    public synchronized boolean getBooleanFromSharedPreferences(String key) {
+    public synchronized boolean getBooleanFromSharedPreferences(final String key) {
         boolean defaultValue = false;
 
         try {
@@ -91,24 +101,26 @@ public class SharedPreferencesManager {
     }
 
     /**
-     * Check if a given key is contained inside of shared preferences
+     * Check if a given key is contained inside of shared preferences.
+     *
      * @param key Key which is being checked
      * @return Boolean indicating whether key is present in shared preferences or not
      */
-    public synchronized boolean isContainedInSharedPreferences(String key) {
+    public synchronized boolean isContainedInSharedPreferences(final String key) {
         return this.sharedPreferences.contains(key);
     }
 
     /**
-     * Remove given key from shared preferences
+     * Remove given key from shared preferences.
+     *
      * @param key Key which might be removed
      */
-    public synchronized void removeFromSharedPreferences(String key) {
+    public synchronized void removeFromSharedPreferences(final String key) {
         this.sharedPreferences.edit().remove(key).apply();
     }
 
     /**
-     * Remove all key-value pairs from shared preferences
+     * Remove all key-value pairs from shared preferences.
      */
     public synchronized void clearSharedPreferences() {
         this.sharedPreferences.edit().clear().apply();
