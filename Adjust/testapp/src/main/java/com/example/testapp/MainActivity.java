36c42db63069700a36710ec4bef021bd2a033735
uerceg <ugi@adjust.com>
2017-12-05 11:07:30 +0100

    Adding more tests to run

--- Adjust/testapp/src/main/java/com/example/testapp/MainActivity.java
+++ Adjust/testapp/src/main/java/com/example/testapp/MainActivity.java
@@ -54,24 +54,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startTestSession() {
-//        testLibrary.setTests("current/sessionCount/Test_SessionCount");
+//        testLibrary.setTests("current/offlineMode/Test_OfflineMode");
 
-        testLibrary.setTests("current/referrer/Test_ReftagReferrer_before_install;" +
-                "current/referrer/Test_ReftagReferrer_before_install_kill_in_between;" +
-                "current/referrer/Test_ReftagReferrer_before_install_uninstall_in_between;" +
-                "current/referrer/Test_ReftagReferrer_after_install;" +
-                "current/referrer/Test_ReftagReferrer_between_create_and_resume;" +
-                "current/referrer/Test_ReftagReferrer_upon_new_session;" +
-                "current/referrer/Test_ReftagReferrer_upon_new_subsession;" +
-                "current/sessionParams/Test_SessionParams_add;" +
-                "current/sessionParams/Test_SessionParams_overwrite;" +
-                "current/sessionParams/Test_SessionParams_remove;" +
-                "current/sessionParams/Test_SessionParams_reset;" +
-                "current/appSecret/Test_AppSecret_no_secret;" +
+        testLibrary.setTests("current/appSecret/Test_AppSecret_no_secret;" +
                 "current/appSecret/Test_AppSecret_with_secret;" +
                 "current/attributionCallback/Test_AttributionCallback_ask_in_multiple;" +
                 "current/attributionCallback/Test_AttributionCallback_ask_in_once;" +
                 "current/attributionCallback/Test_AttributionCallback_no_ask_in;" +
+                "current/deeplink/Test_Deeplink_afterStart_beforeResume;" +
+                "current/deeplink/Test_Deeplink_afterStartAndResume;" +
+                "current/deeplink/Test_Deeplink_beforeResume_restart;" +
+                "current/deeplink/Test_Deeplink_beforeStart;" +
                 "current/defaultTracker/Test_DefaultTracker_empty_string;" +
                 "current/defaultTracker/Test_DefaultTracker_null_string;" +
                 "current/defaultTracker/Test_DefaultTracker_valid_string;" +
@@ -80,23 +73,53 @@ public class MainActivity extends AppCompatActivity {
                 "current/delayStart/Test_DelayStart_delay_5;" +
                 "current/delayStart/Test_DelayStart_delay_5_call_sendFirstPackages;" +
                 "current/delayStart/Test_DelayStart_delay_negative;" +
-                "current/subsessionCount/Test_SubsessionCount;" +
-                "current/userAgent/Test_UserAgent_empty;" +
-                "current/userAgent/Test_UserAgent_not_empty;" +
                 "current/disableEnable/Disable_pushToken_enable;" +
                 "current/disableEnable/Disable_referrer_enable;" +
                 "current/disableEnable/Disable_restart_track;" +
                 "current/disableEnable/Disable_start_enable;" +
                 "current/disableEnable/Disable_track_Enable_track;" +
-                "current/sdkPrefix/Test_SdkPrefix_with_value;" +
-                "current/sdkPrefix/Test_SdkPrefix_empty_value;" +
-                "current/sdkPrefix/Test_SdkPrefix_null_value;" +
+                "current/event/Test_Event_Count_6events;" +
+                "current/event/Test_Event_EventToken_Malformed;" +
+                "current/event/Test_Event_OrderId;" +
+                "current/event/Test_Event_Params;" +
+                "current/event/Test_Event_Revenue_invalid;" +
+                "current/event/Test_Event_Revenue_valid;" +
+                "current/initMalformed/Test_Init_Malformed_empty_appToken;" +
+                "current/initMalformed/Test_Init_Malformed_empty_environment;" +
+                "current/initMalformed/Test_Init_Malformed_missing_appToken;" +
+                "current/initMalformed/Test_Init_Malformed_missing_environment;" +
+                "current/initMalformed/Test_Init_Malformed_wrong_appToken;" +
+                "current/initMalformed/Test_Init_Malformed_wrong_environment;" +
+                "current/offlineMode/Test_OfflineMode;" +
+                "current/referrer/Test_ReftagReferrer_before_install;" +
+                "current/referrer/Test_ReftagReferrer_before_install_kill_in_between;" +
+                "current/referrer/Test_ReftagReferrer_before_install_uninstall_in_between;" +
+                "current/referrer/Test_ReftagReferrer_after_install;" +
+                "current/referrer/Test_ReftagReferrer_between_create_and_resume;" +
+                "current/referrer/Test_ReftagReferrer_upon_new_session;" +
+                "current/referrer/Test_ReftagReferrer_upon_new_subsession;" +
                 "current/sdkInfo/Test_PushToken_after_install;" +
                 "current/sdkInfo/Test_PushToken_before_install;" +
                 "current/sdkInfo/Test_PushToken_before_install_kill_in_between;" +
                 "current/sdkInfo/Test_PushToken_between_create_and_resume;" +
                 "current/sdkInfo/Test_PushToken_multiple_tokens;" +
-                "current/sessionCount/Test_SessionCount");
+                "current/sdkPrefix/Test_SdkPrefix_with_value;" +
+                "current/sdkPrefix/Test_SdkPrefix_empty_value;" +
+                "current/sdkPrefix/Test_SdkPrefix_null_value;" +
+                "current/sendInBackground/Test_SendInBackground_send_false;" +
+                "current/sendInBackground/Test_SendInBackground_send_true;" +
+                "current/sessionCount/Test_SessionCount;" +
+                "current/sessionEventCallbacks/Test_SessionCallback_failure;" +
+                "current/sessionEventCallbacks/Test_SessionCallback_success;" +
+                "current/sessionEventCallbacks/Test_EventCallback_failure;" +
+                "current/sessionEventCallbacks/Test_EventCallback_success;" +
+                "current/sessionParams/Test_SessionParams_add;" +
+                "current/sessionParams/Test_SessionParams_overwrite;" +
+                "current/sessionParams/Test_SessionParams_remove;" +
+                "current/sessionParams/Test_SessionParams_reset;" +
+                "current/subsessionCount/Test_SubsessionCount;" +
+                "current/userAgent/Test_UserAgent_empty;" +
+                "current/userAgent/Test_UserAgent_not_empty");
         // testLibrary.doNotExitAfterEnd();
         testLibrary.initTestSession("android4.12.0");
     }
