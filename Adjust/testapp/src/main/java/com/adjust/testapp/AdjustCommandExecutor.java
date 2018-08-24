9e6458216e4029d9a9fee9ace561ac394ee85101
uerceg <ugi@adjust.com>
2018-08-24 08:38:40 +0200

    Update command executor to handle event callbackId

--- Adjust/testapp/src/main/java/com/adjust/testapp/AdjustCommandExecutor.java
+++ Adjust/testapp/src/main/java/com/adjust/testapp/AdjustCommandExecutor.java
@@ -368,6 +368,7 @@ public class AdjustCommandExecutor {
                     MainActivity.testLibrary.addInfoToSend("timestamp", eventSuccessResponseData.timestamp);
                     MainActivity.testLibrary.addInfoToSend("adid", eventSuccessResponseData.adid);
                     MainActivity.testLibrary.addInfoToSend("eventToken", eventSuccessResponseData.eventToken);
+                    MainActivity.testLibrary.addInfoToSend("callbackId", eventSuccessResponseData.callbackId);
                     if (eventSuccessResponseData.jsonResponse != null ) {
                         MainActivity.testLibrary.addInfoToSend("jsonResponse", eventSuccessResponseData.jsonResponse.toString());
                     }
@@ -387,6 +388,7 @@ public class AdjustCommandExecutor {
                     MainActivity.testLibrary.addInfoToSend("timestamp", eventFailureResponseData.timestamp);
                     MainActivity.testLibrary.addInfoToSend("adid", eventFailureResponseData.adid);
                     MainActivity.testLibrary.addInfoToSend("eventToken", eventFailureResponseData.eventToken);
+                    MainActivity.testLibrary.addInfoToSend("callbackId", eventFailureResponseData.callbackId);
                     MainActivity.testLibrary.addInfoToSend("willRetry", String.valueOf(eventFailureResponseData.willRetry));
                     if (eventFailureResponseData.jsonResponse != null) {
                         MainActivity.testLibrary.addInfoToSend("jsonResponse", eventFailureResponseData.jsonResponse.toString());
@@ -470,6 +472,10 @@ public class AdjustCommandExecutor {
         if (command.parameters.containsKey("orderId")) {
             String orderId = command.getFirstParameterValue("orderId");
             adjustEvent.setOrderId(orderId);
+        }
+        if (command.parameters.containsKey("callbackId")) {
+            String callbackId = command.getFirstParameterValue("callbackId");
+            adjustEvent.setCallbackId(callbackId);
         }
 
 //        Adjust.trackEvent(adjustEvent);
