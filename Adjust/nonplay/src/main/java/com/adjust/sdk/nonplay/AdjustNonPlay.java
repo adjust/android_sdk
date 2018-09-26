e30a5833051d157bcdab76c3f35d64633154284b
nonelse <pedro@adjust.com>
2018-09-26 13:36:44 +0200

    Remove android id and mac address from non play plugin

--- Adjust/nonplay/src/main/java/com/adjust/sdk/nonplay/AdjustNonPlay.java
+++ Adjust/nonplay/src/main/java/com/adjust/sdk/nonplay/AdjustNonPlay.java
@@ -2,18 +2,8 @@ package com.adjust.sdk.nonplay;
 
 public class AdjustNonPlay {
     static boolean isIMEItoBeRead = true;
-    static boolean isAndroidIdToBeRead = true;
-    static boolean isMacAddressToBeRead = true;
 
     public static void readIMEI(boolean isIMEItoBeRead) {
         AdjustNonPlay.isIMEItoBeRead = isIMEItoBeRead;
-    }
-
-    public static void readAndroidIs(boolean isAndroidIdToBeRead) {
-        AdjustNonPlay.isAndroidIdToBeRead = isAndroidIdToBeRead;
-    }
-
-    public static void readMacAddress(boolean isMacAddressToBeRead) {
-        AdjustNonPlay.isMacAddressToBeRead = isMacAddressToBeRead;
     }
 }
--- Adjust/nonplay/src/main/java/com/adjust/sdk/nonplay/Util.java
+++ Adjust/nonplay/src/main/java/com/adjust/sdk/nonplay/Util.java
@@ -1,17 +1,12 @@
 package com.adjust.sdk.nonplay;
 
 import android.content.Context;
-import android.provider.Settings;
-import android.text.TextUtils;
 
 import com.adjust.sdk.ILogger;
-import com.adjust.sdk.Logger;
-import com.adjust.sdk.PackageBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.adjust.sdk.nonplay.MacAddressUtil.injectMacAddress;
 import static com.adjust.sdk.nonplay.TelephonyIdsUtil.injectIMEI;
 
 public class Util {
@@ -19,18 +14,7 @@ public class Util {
         Map<String, String> parameters = new HashMap<String, String>();
 
         injectIMEI(parameters, context, logger);
-        injectAndroidId(parameters, context);
-        injectMacAddress(parameters, context);
 
         return parameters;
-    }
-
-    private static void injectAndroidId(Map<String, String> parameters, Context context) {
-        if (!AdjustNonPlay.isAndroidIdToBeRead) {
-            return;
-        }
-        String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-
-        PackageBuilder.addString(parameters, "android_id", androidId);
     }
 }
