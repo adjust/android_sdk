ce79599699e51725ef22123b1d1432aaf8a2763e
Shashank Suhane <shashank@adjust.com>
2019-11-15 10:49:34 +0100

    Add msa sdk way of reading oaid in oaid plugin

--- Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/AdjustOaid.java
+++ Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/AdjustOaid.java
@@ -1,10 +1,21 @@
 package com.adjust.sdk.oaid;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.bun.miitmdid.core.JLibrary;
+
 public class AdjustOaid {
     static boolean isOaidToBeRead = false;
 
-    public static void readOaid() {
+    public static void readOaid(Context base) {
         AdjustOaid.isOaidToBeRead = true;
+
+        try {
+            JLibrary.InitEntry(base);
+        } catch (NoClassDefFoundError ex) {
+            Log.d("Adjust", "Couldn't find msa sdk " + ex.getMessage());
+        }
     }
 
     public static void doNotReadOaid() {
--- Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/Util.java
+++ Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/Util.java
@@ -13,13 +13,30 @@ public class Util {
     public static Map<String, String> getOaidParameters(Context context, ILogger logger) {
 
         if (AdjustOaid.isOaidToBeRead) {
-            Info oaidInfo = OpenDeviceIdentifierClient.getOaidInfo(context, logger, 1000);
+
+            // IMPORTANT: current logic is to first try reading the oaid with hms (huawei mobile service) approach
+            // which has the capability to return both oaid and limit tracking status
+            // And as a fallback, use the msa sdk approach, which currently only gives the oaid
+
+            Info oaidInfo = OpenDeviceIdentifierClient.getOaidInfo(context, logger, 5000);
             if (oaidInfo != null) {
                 Map<String, String> parameters = new HashMap<String, String>();
                 PackageBuilder.addString(parameters, "oaid", oaidInfo.getOaid());
                 PackageBuilder.addBoolean(parameters, "oaid_tracking_enabled", !oaidInfo.isOaidTrackLimited());
                 return parameters;
             }
+
+            logger.debug("Fail to read the OAID using hms, now try reading it using msa");
+
+            String oaid = MsaSdkClient.getOaid(context, logger, 1000);
+            if (oaid != null) {
+                Map<String, String> parameters = new HashMap<String, String>();
+                PackageBuilder.addString(parameters, "oaid", oaid);
+                return parameters;
+            }
+
+            logger.error("Fail to read the OAID completely");
+
         }
 
         return null;
