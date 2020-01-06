30a9d054c56932d7f9465ccbc1c49aecbbb91de1
uerceg <ugi@adjust.com>
2020-01-06 16:46:06 +0100

    example-app-java cleanup

--- Adjust/example-app-java/src/main/java/com/adjust/examples/ServiceActivity.java
+++ Adjust/example-app-java/src/main/java/com/adjust/examples/ServiceActivity.java
@@ -2,7 +2,7 @@ package com.adjust.examples;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
