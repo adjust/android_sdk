2c0133bf033f06a0188d54bc2198c857bc9611b4
uerceg <ugi@adjust.com>
2020-01-06 16:35:49 +0100

    example-app-fbpixel AndroidX migration and cleanup

--- Adjust/example-app-fbpixel/src/main/java/com/adjust/examples/MainActivity.java
+++ Adjust/example-app-fbpixel/src/main/java/com/adjust/examples/MainActivity.java
@@ -1,8 +1,7 @@
 package com.adjust.examples;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
