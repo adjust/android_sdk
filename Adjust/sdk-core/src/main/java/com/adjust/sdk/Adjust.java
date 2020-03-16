bc33e6fcd44a88afb2c42076f3f443c24a7a661a
uerceg <ugi@adjust.com>
2020-03-16 12:40:39 +0100

    New version 4.21.0

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
@@ -32,7 +32,7 @@ public class Adjust {
      */
     public static synchronized AdjustInstance getDefaultInstance() {
         @SuppressWarnings("unused")
-        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.20.0";
+        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.21.0";
 
         if (defaultInstance == null) {
             defaultInstance = new AdjustInstance();
