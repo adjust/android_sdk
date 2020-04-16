af4e5ba821548734297b7e001c5435d058449ee2
Shashank Suhane <shashank@adjust.com>
2020-04-16 15:10:30 +0200

    Add basic keyboard functionality

---
+++ Adjust/example-app-keyboard/src/main/java/com/adjust/examples/keyboard/KeyboardInputMethodService.java
@@ -0,0 +1,94 @@
+package com.adjust.examples.keyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class KeyboardInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView keyboardView;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        keyboard = new Keyboard(this, R.xml.keyboard);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        if (inputConnection != null) {
+            switch(primaryCode) {
+                case Keyboard.KEYCODE_DELETE :
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+                case Keyboard.KEYCODE_SHIFT:
+                    caps = !caps;
+                    keyboard.setShifted(caps);
+                    keyboardView.invalidateAllKeys();
+                    break;
+                case Keyboard.KEYCODE_DONE:
+                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+
+                    break;
+                default :
+                    char code = (char) primaryCode;
+                    if(Character.isLetter(code) && caps){
+                        code = Character.toUpperCase(code);
+                    }
+                    inputConnection.commitText(String.valueOf(code), 1);
+
+            }
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
--- Adjust/example-app-keyboard/src/main/java/com/adjust/examples/keyboard/MainActivity.java
+++ Adjust/example-app-keyboard/src/main/java/com/adjust/examples/keyboard/MainActivity.java
@@ -1,14 +1,20 @@
 package com.adjust.examples.keyboard;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -28,8 +34,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!isKeyboardEnabled()) {
+            final Context context = this;
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.keyboard_not_enbabled);
+            builder.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                    dialog.dismiss();
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+            builder.setCancelable(false);
+
+            builder.create().show();
+        }
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
+    }
+
+    public boolean isKeyboardEnabled() {
+        try {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            for (final InputMethodInfo imi : imm.getEnabledInputMethodList()) {
+                if (imi.getPackageName().equals(getPackageName())) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            Log.e("example", "Exception while checking if keyboard is enabled", e);
+        }
+        return false;
     }
 
     public void onTrackSimpleEventClick(View v) {
