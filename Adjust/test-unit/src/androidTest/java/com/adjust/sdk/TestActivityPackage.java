04022836287b60c314782fabc09648eb801248fa
ugi <ugi@adjust.com>
2020-07-23 13:50:24 +0200

    new version 4.23.0

--- Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
+++ Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
@@ -53,7 +53,7 @@ public class TestActivityPackage {
         // default values
         appToken = "123456789012";
         environment = "sandbox";
-        clientSdk = "android4.22.0";
+        clientSdk = "android4.23.0";
         suffix = "";
         attribution = new AdjustAttribution();
         playServices = true;
