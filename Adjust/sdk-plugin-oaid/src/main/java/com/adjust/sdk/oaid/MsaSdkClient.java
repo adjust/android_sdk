45574a21a7e2b3beaebbe75929004e4036dc3fc2
Shashank Suhane <shashank@adjust.com>
2020-09-08 11:24:21 +0200

    Update msa sdk version 1.0.23

--- Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/MsaSdkClient.java
+++ Adjust/sdk-plugin-oaid/src/main/java/com/adjust/sdk/oaid/MsaSdkClient.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import com.adjust.sdk.ILogger;
 import com.bun.miitmdid.core.ErrorCode;
 import com.bun.miitmdid.core.MdidSdkHelper;
-import com.bun.supplier.IIdentifierListener;
-import com.bun.supplier.IdSupplier;
+import com.bun.miitmdid.interfaces.IIdentifierListener;
+import com.bun.miitmdid.interfaces.IdSupplier;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
