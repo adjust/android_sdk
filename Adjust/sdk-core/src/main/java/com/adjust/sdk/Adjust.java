3f64317c70a1ea4db914d10593db9825b4b00feb
Shashank Suhane <shashank@adjust.com>
2021-01-27 14:08:00 +0100

    update version number to 4.26.1

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
@@ -32,7 +32,7 @@ public class Adjust {
      */
     public static synchronized AdjustInstance getDefaultInstance() {
         @SuppressWarnings("unused")
-        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.26.0";
+        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.26.1";
 
         if (defaultInstance == null) {
             defaultInstance = new AdjustInstance();
