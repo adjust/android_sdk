a714bfb0e3862c7c39d603adaef7f5b056a86c3f
Shashank Suhane <shashank@adjust.com>
2021-08-06 11:16:09 +0200

    Update version 4.28.4

--- Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
+++ Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
@@ -53,7 +53,7 @@ public class TestActivityPackage {
         // default values
         appToken = "123456789012";
         environment = "sandbox";
-        clientSdk = "android4.28.3";
+        clientSdk = "android4.28.4";
         suffix = "";
         attribution = new AdjustAttribution();
         playServices = true;
