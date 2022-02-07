38913b1c0ba892f570ba618524ccbb931da97aca
Shashank Suhane <shashank@adjust.com>
2022-02-07 16:01:12 +0100

    Update version 4.29.0

--- Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
+++ Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
@@ -211,7 +211,7 @@ var Adjust = {
         if (this.adjustConfig) {
             return this.adjustConfig.getSdkPrefix();
         } else {
-            return 'web-bridge4.28.9';
+            return 'web-bridge4.29.0';
         }
     },
 
