149c9debf52ea209ba1f79e87fbcc0d5818818ae
Shashank Suhane <shashank@adjust.com>
2022-10-18 12:46:49 +0200

    feat: update version number to 4.33.0

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
@@ -34,7 +34,7 @@ public class Adjust {
      */
     public static synchronized AdjustInstance getDefaultInstance() {
         @SuppressWarnings("unused")
-        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.32.0";
+        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.33.0";
 
         if (defaultInstance == null) {
             defaultInstance = new AdjustInstance();
