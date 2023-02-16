e195a129c09a846991741dab4263872668d23c13
Shashank Suhane <shashank@adjust.com>
2023-02-16 09:53:40 +0100

    feat: update version number to 4.33.3

--- Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
+++ Adjust/test-unit/src/androidTest/java/com/adjust/sdk/TestActivityPackage.java
@@ -53,7 +53,7 @@ public class TestActivityPackage {
         // default values
         appToken = "123456789012";
         environment = "sandbox";
-        clientSdk = "android4.33.2";
+        clientSdk = "android4.33.3";
         suffix = "";
         attribution = new AdjustAttribution();
         playServices = true;
