f9bf822d6e1c1bb65baca4f069f68ba1b2a1549f
uerceg <ugi@adjust.com>
2023-06-08 18:16:20 +0200

    feat: update version number to 4.34.0

--- Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
+++ Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
@@ -211,7 +211,7 @@ var Adjust = {
         if (this.adjustConfig) {
             return this.adjustConfig.getSdkPrefix();
         } else {
-            return 'web-bridge4.33.5';
+            return 'web-bridge4.34.0';
         }
     },
 
