b64e60addb8139f860c2a818764f4ccc53294801
Shashank Suhane <shashank@adjust.com>
2023-10-06 22:22:39 +0200

    feat: update version 4.35.1

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Adjust.java
@@ -34,7 +34,7 @@ public class Adjust {
      */
     public static synchronized AdjustInstance getDefaultInstance() {
         @SuppressWarnings("unused")
-        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.35.0";
+        String VERSION = "!SDK-VERSION-STRING!:com.adjust.sdk:adjust-android:4.35.1";
 
         if (defaultInstance == null) {
             defaultInstance = new AdjustInstance();
--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
@@ -30,7 +30,7 @@ public interface Constants {
 
     String SCHEME = "https";
     String AUTHORITY = "app.adjust.com";
-    String CLIENT_SDK = "android4.35.0";
+    String CLIENT_SDK = "android4.35.1";
     String LOGTAG = "Adjust";
     String REFTAG = "reftag";
     String INSTALL_REFERRER = "install_referrer";
