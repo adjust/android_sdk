6c6e5267bcb9fd7b410cf60950272402bfdc039a
Shashank Suhane <shashank@adjust.com>
2023-12-07 14:24:17 +0100

    feat: update version number to 4.37.1

--- Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
+++ Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
@@ -211,7 +211,7 @@ var Adjust = {
         if (this.adjustConfig) {
             return this.adjustConfig.getSdkPrefix();
         } else {
-            return 'web-bridge4.37.0';
+            return 'web-bridge4.37.1';
         }
     },
 
