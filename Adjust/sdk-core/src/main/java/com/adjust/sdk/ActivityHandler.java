d9f2891b315c61aaa9119707085a38067b1179b1
Mahdi ZTD <mahdiztd@gmail.com>
2024-04-12 14:17:12 +0200

    refac: uncomment startConnect of reading of installReferrer

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/ActivityHandler.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/ActivityHandler.java
@@ -1317,7 +1317,7 @@ public class ActivityHandler implements IActivityHandler {
             checkForPreinstallI();
 
             // Try to check if there's new referrer information.
-//            installReferrer.startConnection();
+            installReferrer.startConnection();
             installReferrerMeta.readReferrer();
             readInstallReferrerHuaweiAds();
             readInstallReferrerHuaweiAppGallery();
@@ -1854,7 +1854,7 @@ public class ActivityHandler implements IActivityHandler {
         checkForPreinstallI();
 
         // try to read and send the install referrer
-//        installReferrer.startConnection();
+        installReferrer.startConnection();
         installReferrerMeta.readReferrer();
         readInstallReferrerHuaweiAds();
         readInstallReferrerHuaweiAppGallery();
