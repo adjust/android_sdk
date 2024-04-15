1b2865cf8f9065d764174ceca1fa68a9d17b3dff
Mahdi ZTD <mahdiztd@gmail.com>
2024-04-15 17:40:39 +0200

    refac: add the VivoInstallReferrer async getter create VivoInstallReferrerDetails.java create OnVivoInstallReferrerReadListener.java callback

--- Adjust/sdk-plugin-vivo-referrer/src/main/java/com/adjust/sdk/vivo/AdjustVivoReferrer.java
+++ Adjust/sdk-plugin-vivo-referrer/src/main/java/com/adjust/sdk/vivo/AdjustVivoReferrer.java
@@ -2,13 +2,48 @@ package com.adjust.sdk.vivo;
 
 import android.content.Context;
 
+import com.adjust.sdk.AdjustFactory;
+import com.adjust.sdk.ReferrerDetails;
+import com.adjust.sdk.scheduler.AsyncTaskExecutor;
+
 public class AdjustVivoReferrer {
 
    static boolean shouldReadVivoReferrer = true;
 
-   public static void getVivoInstallReferrer(Context context,OnVivoInstallReferrerReadListener onVivoInstallReferrerReadListener){
+   /**
+    * Called to get Vivo Install Referrer.
+    *
+    * @param context Application context
+    * @param onVivoInstallReferrerReadListener Callback to obtain install referrer.
+    */
+   public static void getVivoInstallReferrer(final Context context, final OnVivoInstallReferrerReadListener onVivoInstallReferrerReadListener) {
+      new AsyncTaskExecutor<Context, ReferrerDetails>() {
+         @Override
+         protected ReferrerDetails doInBackground(Context[] contexts) {
+            ReferrerDetails referrerDetails = null;
+            try {
+               referrerDetails = Util.getVivoInstallReferrerDetails(context, AdjustFactory.getLogger());
+            } catch (Exception exception) {
+               if (onVivoInstallReferrerReadListener == null) {
+                  AdjustFactory.getLogger().error("onVivoInstallReferrerReadListener can not be null");
+                  return null;
+               }
+               onVivoInstallReferrerReadListener.onFail(exception.getMessage());
+            }
+            return referrerDetails;
+         }
 
+         @Override
+         protected void onPostExecute(ReferrerDetails referrerDetails) {
+            if (onVivoInstallReferrerReadListener == null) {
+               AdjustFactory.getLogger().error("onVivoInstallReferrerReadListener can not be null");
+               return;
+            }
+            onVivoInstallReferrerReadListener.onVivoInstallReferrerRead(new VivoInstallReferrerDetails(referrerDetails));
+         }
+      }.execute(context);
    }
+
 
    public static void readVivoReferrer(Context context) {
       shouldReadVivoReferrer = true;
