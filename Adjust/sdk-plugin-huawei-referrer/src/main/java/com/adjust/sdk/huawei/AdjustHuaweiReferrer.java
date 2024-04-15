76ae12336cda26a3d3a1f6930aafa8313e06437c
Mahdi ZTD <mahdiztd@gmail.com>
2024-04-15 14:04:00 +0200

    refac: tuning the getHuaweiInstallReferrer with adding comment and make params final create HuaweiInstallReferrerDetails.java

--- Adjust/sdk-plugin-huawei-referrer/src/main/java/com/adjust/sdk/huawei/AdjustHuaweiReferrer.java
+++ Adjust/sdk-plugin-huawei-referrer/src/main/java/com/adjust/sdk/huawei/AdjustHuaweiReferrer.java
@@ -1,46 +1,72 @@
 package com.adjust.sdk.huawei;
 
-import static com.adjust.sdk.Constants.REFERRER_API_HUAWEI_ADS;
-import static com.adjust.sdk.Constants.REFERRER_API_HUAWEI_APP_GALLERY;
-
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
 
-import com.adjust.sdk.Adjust;
 import com.adjust.sdk.AdjustFactory;
 import com.adjust.sdk.ReferrerDetails;
+import com.adjust.sdk.scheduler.AsyncTaskExecutor;
 
 public class AdjustHuaweiReferrer {
 
    static boolean shouldReadHuaweiReferrer = true;
 
-   public static void getHuaweiInstallReferrer(Context context, OnHuaweiInstallReferrerReadListener onHuaweiInstallReferrerReadListener){
-      Handler handlerAds = new Handler(Looper.getMainLooper());
-      Handler handlerAppGallery = new Handler(Looper.getMainLooper());
-      handlerAds.post(new Runnable() {
+   /**
+    * Called to get Huawei Install Referrer.
+    *
+    * @param context Application context
+    * @param onHuaweiInstallReferrerReadListener Callback to obtain install referrer.
+    */
+   public static void getHuaweiInstallReferrer(final Context context, final OnHuaweiInstallReferrerReadListener onHuaweiInstallReferrerReadListener) {
+      new AsyncTaskExecutor<Context, ReferrerDetails>() {
          @Override
-         public void run() {
+         protected ReferrerDetails doInBackground(Context[] contexts) {
+            ReferrerDetails referrerDetails = null;
             try {
-               ReferrerDetails referrerDetails = Util.getHuaweiAdsInstallReferrerDetails(context, AdjustFactory.getLogger());
-               onHuaweiInstallReferrerReadListener.onHuaweiAdsInstallReferrerDetailsRead(referrerDetails);
-            }catch (Exception exception){
-               onHuaweiInstallReferrerReadListener.onFailure(exception.getMessage());
+               referrerDetails = Util.getHuaweiAppGalleryInstallReferrerDetails(context, AdjustFactory.getLogger());
+            } catch (Exception exception) {
+               if (onHuaweiInstallReferrerReadListener == null) {
+                  AdjustFactory.getLogger().error("onHuaweiInstallReferrerReadListener can not be null");
+                  return null;
+               }
+               onHuaweiInstallReferrerReadListener.onFail(exception.getMessage());
             }
+            return referrerDetails;
          }
-      });
-      handlerAppGallery.post(new Runnable() {
-         @Override
-         public void run() {
-            try {
-               ReferrerDetails referrerDetails = Util.getHuaweiAppGalleryInstallReferrerDetails(context, AdjustFactory.getLogger());
-               onHuaweiInstallReferrerReadListener.onHuaweiAppGalleryInstallReferrerDetailsRead(referrerDetails);
-            }catch (Exception exception){
-               onHuaweiInstallReferrerReadListener.onFailure(exception.getMessage());
-            }
-         }
-      });
 
+         @Override
+         protected void onPostExecute(ReferrerDetails referrerDetails) {
+            if (onHuaweiInstallReferrerReadListener == null) {
+               AdjustFactory.getLogger().error("onHuaweiInstallReferrerReadListener can not be null");
+               return;
+            }
+            onHuaweiInstallReferrerReadListener.onHuaweiAppGalleryInstallReferrerDetailsRead(new HuaweiInstallReferrerDetails(referrerDetails));
+         }
+      }.execute(context);
+      new AsyncTaskExecutor<Context, ReferrerDetails>() {
+         @Override
+         protected ReferrerDetails doInBackground(Context[] contexts) {
+            ReferrerDetails referrerDetails = null;
+            try {
+               referrerDetails = Util.getHuaweiAdsInstallReferrerDetails(context, AdjustFactory.getLogger());
+            } catch (Exception exception) {
+               if (onHuaweiInstallReferrerReadListener == null) {
+                  AdjustFactory.getLogger().error("onHuaweiInstallReferrerReadListener can not be null");
+                  return null;
+               }
+               onHuaweiInstallReferrerReadListener.onFail(exception.getMessage());
+            }
+            return referrerDetails;
+         }
+
+         @Override
+         protected void onPostExecute(ReferrerDetails referrerDetails) {
+            if (onHuaweiInstallReferrerReadListener == null) {
+               AdjustFactory.getLogger().error("onHuaweiInstallReferrerReadListener can not be null");
+               return;
+            }
+            onHuaweiInstallReferrerReadListener.onHuaweiAdsInstallReferrerDetailsRead(new HuaweiInstallReferrerDetails(referrerDetails));
+         }
+      }.execute(context);
    }
 
    public static void readHuaweiReferrer(Context context) {
