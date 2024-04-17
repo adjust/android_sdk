d5b62785870b26f98673b2302f67e1e6197fc807
uerceg <ugi@adjust.com>
2024-04-17 15:27:23 +0200

    refac: stop preventing tps tracking if coppa is enabled

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/ActivityHandler.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/ActivityHandler.java
@@ -2425,11 +2425,6 @@ public class ActivityHandler implements IActivityHandler {
         }
         if (!isEnabledI()) { return; }
         if (activityState.isGdprForgotten) { return; }
-        SharedPreferencesManager sharedPreferencesManager = SharedPreferencesManager.getDefaultInstance(getContext());
-        if (sharedPreferencesManager.getCoppaCompliance()) {
-            logger.warn("Calling third party sharing API not allowed when COPPA enabled");
-            return;
-        }
 
         long now = System.currentTimeMillis();
         PackageBuilder packageBuilder = new PackageBuilder(
