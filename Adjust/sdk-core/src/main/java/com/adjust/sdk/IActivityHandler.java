91c3f361500f760b2f347633bc99e20639851a89
Mahdi ZTD <mahdiztd@gmail.com>
2024-05-13 17:37:39 +0200

    style:refactor isEnabled to async getter

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/IActivityHandler.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/IActivityHandler.java
@@ -20,6 +20,7 @@ public interface IActivityHandler {
     void setEnabled(boolean enabled);
 
     boolean isEnabled();
+    void getIsEnabled(OnIsEnabledListener onIsEnabledListener);
 
     void readOpenUrl(Uri url, long clickTime);
 
