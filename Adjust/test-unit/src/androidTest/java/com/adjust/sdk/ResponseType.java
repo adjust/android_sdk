f47001daf459fa5e7721547f348577153743e603
Mahdi ZTD <mahdiztd@gmail.com>
2024-05-17 11:35:56 +0200

    refac:remove test-unit module

--- Adjust/test-unit/src/androidTest/java/com/adjust/sdk/ResponseType.java
+++
@@ -1,8 +0,0 @@
-package com.adjust.sdk;
-
-/**
- * Created by pfms on 28/01/15.
- */
-public enum ResponseType {
-    NULL, CLIENT_PROTOCOL_EXCEPTION, INTERNAL_SERVER_ERROR, WRONG_JSON, EMPTY_JSON, MESSAGE;
-}
