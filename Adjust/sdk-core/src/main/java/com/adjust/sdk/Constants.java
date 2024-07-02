95e4f71300838bcfa2bd429bc54f83d376666a34
Shashank Suhane <shashank@adjust.com>
2024-07-02 13:31:30 +0200

    feat: update version number to 4.38.5

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
@@ -30,7 +30,7 @@ public interface Constants {
 
     String SCHEME = "https";
     String AUTHORITY = "app.adjust.com";
-    String CLIENT_SDK = "android4.38.4";
+    String CLIENT_SDK = "android4.38.5";
     String LOGTAG = "Adjust";
     String REFTAG = "reftag";
     String INSTALL_REFERRER = "install_referrer";
--- Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
+++ Adjust/sdk-plugin-webbridge/src/main/assets/adjust.js
@@ -211,7 +211,7 @@ var Adjust = {
         if (this.adjustConfig) {
             return this.adjustConfig.getSdkPrefix();
         } else {
-            return 'web-bridge4.38.4';
+            return 'web-bridge4.38.5';
         }
     },
 
