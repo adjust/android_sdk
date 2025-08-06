859620c5cb7f793d00338f3b9d29ddaaed81bde4
Shashank Suhane <shashank@adjust.com>
2025-08-06 14:46:21 +0200

    feat: update version number to 5.4.2

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/Constants.java
@@ -38,7 +38,7 @@ public interface Constants {
 
     String SCHEME = "https";
     String AUTHORITY = "app.adjust.com";
-    String CLIENT_SDK = "android5.4.1";
+    String CLIENT_SDK = "android5.4.2";
     String LOGTAG = "Adjust";
     String REFTAG = "reftag";
     String INSTALL_REFERRER = "install_referrer";
